225c33cb72f0e8abe759b442b950ac9a12b1fe9b
jgchoiqa <jgchoi.qa@gmail.com>
2017-02-06 14:40:33 +0900

    TipCalculator 작성

--- src/Joshua/CT/Exercise1/TipCalculator.java
+++ src/Joshua/CT/Exercise1/TipCalculator.java
@@ -1,7 +1,24 @@
 package Joshua.CT.Exercise1;
 
+import java.util.Scanner;
+
 /**
  * Created by jgchoi.qa on 2017. 2. 6..
  */
 public class TipCalculator {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("bill amount : ");
+        float billAmount = input.nextFloat();
+        System.out.print("tip rate : ");
+        int tipRate = input.nextInt();
+        float tip, total;
+
+        tip = (float)((tipRate * 0.01) * billAmount);
+        total = billAmount + tip;
+
+        System.out.printf("tip : %.2f\n", tip);
+        System.out.printf("total : %.2f", total);
+    }
 }

69a554492503886ee32f617afb7a26b28b8bea62
joshua <jgchoi.qa@gmail.com>
2017-02-07 13:52:58 +0900

    StringLength 주석 추가 (도전과제 1번)

--- src/Joshua/CT/Exercise2/StringLength.java
+++ src/Joshua/CT/Exercise2/StringLength.java
@@ -14,6 +14,7 @@ public class StringLength {
         System.out.print("What is the input string? ");
         String name = input.nextLine();
 
+        // 도전 과제 1 : 아무 것도 입력하지 않았을 때 검증하기
         while (!flag) {
             if (name.length() <= 0) {
                 System.out.print("Invalid input error. what is the input string? ");
